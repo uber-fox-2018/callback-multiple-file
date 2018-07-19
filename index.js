24bfbc8959ecae1e07ccae3d4e0c7a40c2c7feb2
minzarddillah <minzard.dillah@gmail.com>
2018-07-19 21:09:47 +0700

    update

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
+
 const fs = require('fs');
 const sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
+function match_data(parent_file, children_file, cb) {
   // Code here
   fs.readFile(parent_file,'utf8',(err,data) =>{
-    var parent_file = JSON.parse(data)
-    sleep.sleep(3)
-    fs.readFile(children_file,'utf8',(err,data) =>{
-      var children_file = JSON.parse(data)
-      for(let i = 0 ; i < parent_file.length ; i++){
-        parent_file[i].children = []
-        for(let j = 0 ; j < children_file.length ; j++){
-          if(parent_file[i].last_name === children_file[j].family){
-            parent_file[i].children.push(children_file[j].full_name)
+    if(err){
+      console.log('ciee yang lagi nyariin bapak')
+    }else{
+      var parent_file = JSON.parse(data)
+      sleep.sleep(3)
+      fs.readFile(children_file,'utf8',(err,data) =>{
+        if(err) {
+          console.log(`Salah lu`)
+        } else {
+          var children_file = JSON.parse(data)
+          for(let i = 0 ; i < parent_file.length ; i++){
+            parent_file[i].children = []
+            for(let j = 0 ; j < children_file.length ; j++){
+              if(parent_file[i].last_name === children_file[j].family){
+                parent_file[i].children.push(children_file[j].full_name)
+              }
+            }
           }
+          // console.log(parent_file)
+          cb(parent_file)
         }
-      }
-      console.log(parent_file)
-    })
+      })
+    }
+   
   })
 }
 
@@ -26,5 +37,10 @@ function match_data(parent_file, children_file) {
 //   console.log(parent_file)
 // }
 
-match_data('./parents.json', './children.json')
+function cb(data) {
+  console.log(data);
+  
+}
+
+match_data('./parentss.json', './children.json', cb)
 console.log("Notification : Data sedang diproses !");
