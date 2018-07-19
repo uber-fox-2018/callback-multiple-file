f3553f420bc8e99706642cf8e5d3367475c06f03
riadji.github.io <riadji.ramadhan@gmail.com>
2018-07-19 20:33:42 +0700

    Add callback Multiple file

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const fs = require('fs');
 const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  // Code here
+  fs.readFile(parent_file, 'utf-8', (err, data) => {
+    console.log(data);
+  })
+
 }
 
 match_data('./parents.json', './children.json')
 console.log("Notification : Data sedang diproses !");
+
+// readFile() {
+//   let fileStr = fs.readFileSync('./data.json', 'utf-8')
+//   this.dataJson = JSON.parse(fileStr)
+// }
