58303b5869e9c9cfd870dda61aba3089effb0657
harlesbayu <bayuanggaraharles@gmail.com>
2018-07-19 19:32:41 +0700

    harlesbayuanggara

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
   // Code here
+  console.log("Notification : Data sedang diproses !");
+  sleep.sleep(5)
+  
+  fs.readFile(parent_file, 'utf-8',(err,data) => {
+    let parent = JSON.parse(data)
+    
+    fs.readFile(children_file, 'utf-8', (err,data) => {
+       let children = JSON.parse(data)
+       for(let i = 0; i < parent.length; i++){
+        parent[i].children = []
+          for(let j = 0; j < children.length; j++){
+              if(parent[i].last_name == children[j].family){
+                  parent[i].children.push(children[j].full_name)
+              }
+          }
+       }
+       console.log(parent)
+       
+    })
+  })
 }
 
+
 match_data('./parents.json', './children.json')
-console.log("Notification : Data sedang diproses !");
+
