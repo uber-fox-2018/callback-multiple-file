04c965dac817cb47ec5d5527c0fcac811284af0d
Susan Nio <susan.wen88@gmail.com>
2018-07-19 17:18:58 +0700

    Adding index.js

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const fs = require('fs');
 const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  // Code here
+  fs.readFile(parent_file, 'utf8', function(err,parent_data){
+      let parentStr = parent_data;
+      let parents = JSON.parse(parentStr);
+      sleep.sleep(5);
+      fs.readFile(children_file, 'utf8', function(err,children_data){
+        let childrenStr = children_data;
+        let children = JSON.parse(childrenStr);
+        sleep.sleep(5);
+        for(let parent of parents){
+          parent.children = [];
+          for(let child of children){
+            if(child.family === parent.last_name){
+              parent.children.push(child.full_name);
+            }
+          }
+        }
+        console.log(parents);
+       
+     })
+  })
 }
-
 match_data('./parents.json', './children.json')
 console.log("Notification : Data sedang diproses !");
