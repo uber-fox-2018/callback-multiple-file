333c3193be48ac3bdbd744f14c4ad7bd84bf1072
riadji.github.io <riadji.ramadhan@gmail.com>
2018-07-19 22:34:36 +0700

    Riadji Add MultipleFile

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
   fs.readFile(parent_file, 'utf-8', (err, data) => {
-    console.log(data);
+    sleep.sleep(5)
+    let parentFiles = JSON.parse(data)
+    fs.readFile(children_file, 'utf-8', (err, data) => {
+      let childrenFiles = JSON.parse(data)
+        for (let i = 0; i < parentFiles.length; i++) {
+          parentFiles[i]['children'] = []
+          for (let j = 0; j < childrenFiles.length; j++) {
+            if (childrenFiles[j]['family'] === parentFiles[i]['last_name']) {
+                parentFiles[i]['children'].push(childrenFiles[j]['full_name']);
+            }
+          }
+        }
+        display(parentFiles)
+    })
   })
 
+}
+
+function display (callBack) {
+  console.log(callBack)
 }
 
 match_data('./parents.json', './children.json')
