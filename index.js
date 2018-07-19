dc34d5b0ef40c30517c024dc54f808730e9102fb
hacktiv8 <hacktiv8@H8JKT009.localdomain>
2018-07-19 17:26:29 +0700

    refactor

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ const fs = require('fs');
 const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  fs.readFile(parent_file, 'utf8', (err, parents) => {
+  parser (parent_file, (parents) => {
     console.log('data parent sedang diproses...');
     sleep.sleep(5);
-    if(err){
+    parser (children_file, (children) => {
+      console.log('data children sedang diproses...');
+      sleep.sleep(5);
+      parents.forEach((parent) => {
+        let childrenArr = children.filter((child) => {
+          return child.family == parent.last_name;
+        })
+        parent.children = childrenArr;
+        console.log(parent);
+      })
+    })
+  })
+}
+
+function parser (path, cb){
+  fs.readFile(path, 'utf8', (err, result) => {
+    if (err){
       console.log(err.message);
     } else {
-      fs.readFile(children_file, 'utf8', (err, children) => {
-        console.log('data children sedang diproses...');
-        sleep.sleep(5);
-        if (err){
-          console.log(err.message);
-        } else {
-          JSON.parse(parents).forEach((parent) => {
-            let childrenArr = JSON.parse(children).filter((child) => {
-              return child.family == parent.last_name;
-            })
-            parent.children = childrenArr;
-            console.log(parent)
-          })
-        }
-      })
+      cb (JSON.parse(result))
     }
   })
 }
