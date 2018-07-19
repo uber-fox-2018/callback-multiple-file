5365061e889296f1ff04decfe3c357f96fe81261
imamaguss <imam.agussopian@gmail.com>
2018-07-19 21:32:39 +0700

    imam add callback-multiple-file

--- index.js
+++ index.js
@@ -2,8 +2,45 @@ const fs = require('fs');
 const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  // Code here
+  fs.readFile(parent_file, 'utf8', (err, data) => {
+    sleep.sleep(2);
+    if(err) {
+      console.log(`Something has trouble!`);
+    }
+    
+    let parentData = JSON.parse(data);
+    fs.readFile(children_file, 'utf8', (err, data) => {
+      if(err) {
+        console.log(`Somtehing has trouble!`);
+      }
+      let childrenData = JSON.parse(data);
+      for(let i = 0; i < parentData.length; i++) {
+        parentData[i].children = [];
+        for(let j = 0; j < childrenData.length; j++) {
+          if(parentData[i].last_name === childrenData[j].family) {
+            parentData[i].children.push(childrenData[j].full_name);
+          }
+        }
+      }
+      callback(parentData);
+    });
+  });
+}
+
+function callback(parentData) {
+  console.log(parentData);
+  
 }
 
 match_data('./parents.json', './children.json')
 console.log("Notification : Data sedang diproses !");
+
+
+// var content;
+// fs.readFile('./Index.html', function read(err, data) {
+//     if (err) {
+//         throw err;
+//     }
+//     content = data;
+// });
+// console.log(content);
