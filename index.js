d1ae9e547805d7fcce99c4602b18a5e36ffa28bd
gandryeanb <gstandrianb@gmail.com>
2018-07-19 20:15:30 +0700

    adding js file

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const fs = require('fs');
 const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  // Code here
+  let parent = fs.readFile(parent_file,`utf8`,(err,dataParent) => {
+    let dataParentJson = JSON.parse(dataParent)
+    let children = fs.readFile(children_file,`utf8`,(err,dataChildren) => {
+      let dataChildrenJson = JSON.parse(dataChildren)
+      for (let i = 0; i < dataParentJson.length; i++) {
+        dataParentJson[i].children = []
+        for (let j = 0; j < dataChildrenJson.length; j++) {
+          if (dataParentJson[i].last_name === dataChildrenJson[j].family) {
+            dataParentJson[i].children.push(dataChildrenJson[j].full_name)
+          }
+        }
+      }
+      console.log(dataParentJson)
+    })
+  })
 }
 
 match_data('./parents.json', './children.json')
