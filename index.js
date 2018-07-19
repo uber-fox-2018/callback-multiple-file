c9f5ab421f74e50bc4b497e319339d4cbab5aafb
hacktiv8 <hacktiv8@H8JKT009.localdomain>
2018-07-19 17:10:12 +0700

    fixed output

--- index.js
+++ index.js
@@ -2,30 +2,31 @@ const fs = require('fs');
 const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  console.log('data parent sedang diproses...');
-  // sleep.sleep(5);
   fs.readFile(parent_file, 'utf8', (err, parents) => {
+    console.log('data parent sedang diproses...');
+    sleep.sleep(5);
     if(err){
       console.log(err.message);
     } else {
-      console.log('data children sedang diproses...');
-      // sleep.sleep(5);
       fs.readFile(children_file, 'utf8', (err, children) => {
+        console.log('data children sedang diproses...');
+        sleep.sleep(5);
         if (err){
           console.log(err.message);
         } else {
-          let output = JSON.parse(parents).forEach((parent) => {
+          JSON.parse(parents).forEach((parent) => {
             let childrenArr = JSON.parse(children).filter((child) => {
               return child.family == parent.last_name;
             })
             parent.children = childrenArr;
+            console.log(parent)
           })
-          console.log(output)
         }
       })
     }
   })
 }
+
 
 match_data('./parents.json', './children.json')
 console.log("Notification : Data sedang diproses !");
