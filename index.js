8d9553701d131adf306396d63411c97938fb22de
HelmiYogantara <helmiyogantara@gmail.com>
2018-07-19 21:18:00 +0700

    done

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile(parent_file,function(err,data){
+    var parent = JSON.parse(data)
+    fs.readFile(children_file,function(err,data){
+      var children = JSON.parse(data)
+      for(var i = 0; i < parent.length;i++){
+        parent[i].children = []
+        for(var j = 0; j < children.length;j++){
+          if(parent[i].last_name === children[j].family){
+            parent[i].children.push(children[j].full_name)
+            console.log(parent[i]);
+            sleep.sleep(1)        
+            
+          }
+        }
+      }
+    })
+  })
+  
 }
 
 match_data('./parents.json', './children.json')
