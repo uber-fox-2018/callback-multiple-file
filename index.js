e0b80c5ec33b2963c6819d63d6323fddd03fa66d
sbachr <sbachr0501@gmail.com>
2018-07-19 19:51:49 +0700

    father who finding his childe

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile('./parents.json', 'utf8', function (err, parent) {
+    if (err) throw err
+    else parent = JSON.parse(parent)
+    fs.readFile('./children.json', 'utf8', function (err, children) {
+      if (err) throw err
+      else children = JSON.parse(children)
+    
+      let searchChild = []
+      for (let a = 0; a < parent.length; a++) {
+        let temp = []
+        for (let b = 0; b < children.length; b++) {
+          if (parent[a].last_name == children[b].family) temp.push(children[b].full_name)
+        }
+        parent[a].children = temp
+      }
+      console.log(parent)
+    })
+  })
+
 }
 
 match_data('./parents.json', './children.json')
