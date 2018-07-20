5c31faaddd21a1a7755704e9b337814a3a17ffbb
Andre Sudi Yudatama <andresudi@DDCBU-elee.localdomain>
2018-07-20 16:30:02 +0700

    salah kirim tugas

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const fs = require('fs');
-const sleep = require('sleep');
+var sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
-  // Code here
-}
+  // your code here...
 
-match_data('./parents.json', './children.json')
+  fs.readFile(parent_file, function(err,data) {
+    if (err) {
+      throw err
+    } 
+    let parentData = JSON.parse(data)
+    sleep.sleep(5)
+    fs.readFile(children_file, function(err,data) {
+      if (err) {
+        throw err
+      } 
+      let childrenData = JSON.parse(data)
+      let output = []
+      for (let i = 0; i < parentData.length; i++) {
+        let newArr = []
+          for (let j = 0; j < childrenData.length; j++) {
+            if (childrenData[j].family === parentData[i].last_name) {
+              newArr.push(childrenData[j].full_name)
+            }
+          }
+          let obj = {
+            id: parentData[i].id,
+            full_name: parentData[i].full_name,
+            last_name: parentData[i].last_name,
+            age: parentData[i].age,
+            children: newArr
+          }
+          output.push(obj)
+        }
+        console.log(obj)
+      })
+    }) 
+  }     
+
+
+
+match_data('./parents.json', './childrens.json')
 console.log("Notification : Data sedang diproses !");
